106a3c98421fbff39dbf7e21cb1b8260acc0e7bb
Jens Fischer <jensfischer95@gmail.com>
2017-12-01 10:12:11 +0100

    Part 2 passing

--- src/Day1.hx
+++ src/Day1.hx
@@ -4,41 +4,42 @@ class Day1 extends buddy.SingleSuite {
     function new() {
         describe("Day1", {
             it("part1", {
-                sum("1122").should.be(3);
-                sum("1111").should.be(4);
-                sum("1234").should.be(0);
-                sum("91212129").should.be(9);
+                sum1("1122").should.be(3);
+                sum1("1111").should.be(4);
+                sum1("1234").should.be(0);
+                sum1("91212129").should.be(9);
             });
 
             it("part2", {
-                sum("1212").should.be(6);
-                sum("1221").should.be(0);
-                sum("123425").should.be(4);
-                sum("123123").should.be(12);
-                sum("12131415").should.be(4);
+                sum2("1212").should.be(6);
+                sum2("1221").should.be(0);
+                sum2("123425").should.be(4);
+                sum2("123123").should.be(12);
+                sum2("12131415").should.be(4);
             });
         });
     }
 
-    function sum(input:String):Int {
+    function sum(input:String, step:Int):Int {
         var sum = 0;
         var a = (input : CircularAccess);
         for (i in 0...input.length) {
-            if (a[i] == a[i + 1]) {
+            if (a[i] == a[i + step]) {
                 sum += Std.parseInt(a[i]);
             }
         }
         return sum;
     }
 
-    function sum2(input:String):Int {
-        return 0;
-    }
+    function sum1(input:String):Int
+        return sum(input, 1);
+
+    function sum2(input:String):Int
+        return sum(input, Std.int(input.length / 2));
 }
 
 abstract CircularAccess(String) from String {
     @:arrayAccess function get(i:Int):String {
-        i %= this.length;
-        return this.charAt(i);
+        return this.charAt(i % this.length);
     }
 }
