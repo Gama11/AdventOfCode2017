649c7b50075d7fbdd52dfdf400348f959195ff6b
Jens Fischer <jensfischer95@gmail.com>
2017-12-04 11:22:05 +0100

    Part 2 passing

--- src/Day4.hx
+++ src/Day4.hx
@@ -5,31 +5,55 @@ class Day4 extends buddy.SingleSuite {
     function new() {
         describe("Day4", {
             it("part1", {
-                isPassphraseValid("aa bb cc dd ee").should.be(true);
-                isPassphraseValid("aa bb cc dd aa").should.be(false);
-                isPassphraseValid("aa bb cc dd aaa").should.be(true);
+                isPassphraseValid1("aa bb cc dd ee").should.be(true);
+                isPassphraseValid1("aa bb cc dd aa").should.be(false);
+                isPassphraseValid1("aa bb cc dd aaa").should.be(true);
 
-                getValidPassphraseCount(
+                getValidPassphraseCount(isPassphraseValid1,
 "aa bb cc dd ee
 aa bb cc dd aa
 aa bb cc dd aaa").should.be(2);
             });
+
+            it("part2", {
+                isPassphraseValid2("abcde fghij").should.be(true);
+                isPassphraseValid2("abcde xyz ecdab").should.be(false);
+                isPassphraseValid2("a ab abc abd abf abj").should.be(true);
+                isPassphraseValid2("iiii oiii ooii oooi oooo").should.be(true);
+                isPassphraseValid2("oiii ioii iioi iiio").should.be(false);
+            });
         });
     }
 
-    function isPassphraseValid(passphrase:String):Bool {
-        passphrase = passphrase.trim();
-        var words = new Map<String, Bool>();
-        for (word in ~/[ \t]+/g.split(passphrase)) {
-            if (words.exists(word)) {
+    function getWords(s:String):Array<String> {
+        return ~/[ \t]+/g.split(s.trim());
+    }
+
+    function isPassphraseValid(words:Array<String>):Bool {
+        var wordsSoFar = new Map<String, Bool>();
+        for (word in words) {
+            if (wordsSoFar.exists(word)) {
                 return false;
             }
-            words[word] = true;
+            wordsSoFar[word] = true;
         }
         return true;
     }
 
-    function getValidPassphraseCount(passphrases:String):Int {
-        return passphrases.split("\n").map(isPassphraseValid).filter(valid -> valid).length;
+    function isPassphraseValid1(passphrase:String):Bool {
+        return isPassphraseValid(getWords(passphrase));
+    }
+
+    function isPassphraseValid2(passphrase:String):Bool {
+        var words = getWords(passphrase).map(word -> {
+            var characters = word.split("");
+            characters.sort(Reflect.compare);
+            return characters.join("");
+        });
+        return isPassphraseValid(words);
+    }
+
+    function getValidPassphraseCount(isValid:String->Bool, passphrases:String):Int {
+        return passphrases.split("\n").map(isValid).filter(valid -> valid).length;
     }
 }
