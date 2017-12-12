470c7208a1742de7eddf8b7b04aba6ac480b46dc
Jens Fischer <jensfischer95@gmail.com>
2017-12-12 12:19:54 +0100

    Day 12 part 2

--- src/Day12.hx
+++ src/Day12.hx
@@ -3,15 +3,21 @@ using buddy.Should;
 class Day12 extends buddy.SingleSuite {
     function new() {
         describe("Day12", {
-            it("part1", {
-                getGroupZero(
+            var data =
 "0 <-> 2
 1 <-> 1
 2 <-> 0, 3, 4
 3 <-> 2, 4
 4 <-> 2, 3, 6
 5 <-> 6
-6 <-> 4, 5").length.should.be(6);
+6 <-> 4, 5";
+
+            it("part1", {
+                getGroupZero(data).length.should.be(6);
+            });
+
+            it("part2", {
+                countGroups(data).should.be(2);
             });
         });
     }
@@ -28,19 +34,36 @@ class Day12 extends buddy.SingleSuite {
     }
 
     function getGroupZero(input:String):Array<Int> {
-        var connections = parseConnections(input);
+        return getGroup(parseConnections(input), 0);
+    }
+
+    function getGroup(connections:Connections, start:Int):Array<Int> {
         function collectConnections(start:Int, visited:Array<Int>):Array<Int> {
             var connection = connections[start];
             var subPrograms = [start];
             for (program in connection) {
                 if (visited.indexOf(program) == -1) {
                     visited.push(program);
-                    subPrograms = subPrograms.concat(collectConnections(program, visited));
+                    subPrograms = subPrograms.concat(
+                        collectConnections(program, visited));
                 }
             }
             return subPrograms;
         }
-        return collectConnections(0, [0]);
+        return collectConnections(start, [start]);
+    }
+
+    function countGroups(input:String) {
+        var connections = parseConnections(input);
+        var visitedPrograms = [];
+        var groups = 0;
+        for (program in connections.keys()) {
+            if (visitedPrograms.indexOf(program) == -1) {
+                visitedPrograms = visitedPrograms.concat(getGroup(connections, program));
+                groups++;
+            }
+        }
+        return groups;
     }
 }
 
