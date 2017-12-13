68fe1612668b3f09e536f64b1811971e537a5db0
Jens Fischer <jensfischer95@gmail.com>
2017-12-13 13:13:53 +0100

    Day 13 part 2

--- src/Day13.hx
+++ src/Day13.hx
@@ -1,53 +1,78 @@
 import util.IntMath;
 using buddy.Should;
+using Lambda;
 
 class Day13 extends buddy.SingleSuite {
     function new() {
         describe("Day13", {
-            it("part1", {
-                calculateTripSeverity(
+            var input =
 "0: 3
 1: 2
 4: 4
-6: 4"
-                ).should.be(24);
+6: 4";
+
+            it("part1", {
+                calculateTripSeverity(input).should.be(24);
+            });
+
+            it("part2", {
+                findMinimumDelay(input).should.be(10);
             });
         });
     }
 
-    function calculateTripSeverity(input:String):Int {
-        var scanners = new Map<Int, Scanner>();
+    function parseInput(input:String):Array<Scanner> {
+        var scanners = [];
         var maxDepth = 0;
         for (line in input.split("\n")) {
             var properties = line.split(": ").map(Std.parseInt);
             var depth = properties[0];
             var range = properties[1];
-            scanners[depth] = {range: range, position: 0, direction: 1};
+            scanners[depth] = {depth: depth, range: range};
 
             maxDepth = IntMath.max(maxDepth, depth);
         }
+        return scanners;
+    }
 
-        var severity = 0;
-        for (depth in 0...maxDepth + 1) {
+    function findScannerCollisions(scanners:Array<Scanner>, delay:Int = 0):Array<Scanner> {
+        var collisions = [];
+        for (depth in 0...scanners.length + 1) {
             var scanner = scanners[depth];
-            if (scanner != null && scanner.position == 0) {
-                severity += depth * scanner.range;
+            var time = depth + delay;
+            if (scanner != null && calculateScannerPosition(time, scanner.range) == 0) {
+                collisions.push(scanner);
             }
-
-            for (scanner in scanners) {
-                scanner.position += scanner.direction;
-                if (scanner.position <= 0 || scanner.position >= scanner.range - 1) {
-                    scanner.direction *= -1;
-                }
-            }
-            
         }
-        return severity;
+        return collisions;
+    }
+
+    function calculateScannerPosition(time:Int, range:Int):Int {
+        var maxPosition = range - 1;
+        var rounds = Std.int(time / maxPosition);
+        var steps = time % maxPosition;
+        return if (rounds % 2 == 0) steps else maxPosition - steps;
+    }
+
+    function calculateTripSeverity(input:String):Int {
+        return findScannerCollisions(parseInput(input))
+            .map(scanner -> scanner.depth * scanner.range)
+            .fold((a, b) -> a + b, 0);
+    } 
+
+    function findMinimumDelay(input:String):Int {
+        var scanners = parseInput(input);
+        var delay = 0;
+        while (true) {
+            if (findScannerCollisions(scanners, delay).length == 0) {
+                return delay;
+            }
+            delay++;
+        } 
     }
 }
 
 typedef Scanner = {
+    final depth:Int;
     final range:Int;
-    var position:Int;
-    var direction:Int;
 }
