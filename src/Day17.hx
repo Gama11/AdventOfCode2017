34191a6e7d92af2eb58719ac7ac7970a6fb7b1b2
Jens Fischer <jensfischer95@gmail.com>
2017-12-17 11:32:57 +0100

    Day 17 part 2

--- src/Day17.hx
+++ src/Day17.hx
@@ -1,5 +1,4 @@
 using buddy.Should;
-using StringTools;
 
 class Day17 extends buddy.SingleSuite {
     function new() {
@@ -23,5 +22,20 @@ class Day17 extends buddy.SingleSuite {
 
     function getValueAfter2017(buffer:Array<Int>):Int {
         return buffer[(buffer.indexOf(2017) + 1) % buffer.length];
+    }
+
+    function spinlock2(stride:Int):Int {
+        var bufferLength = 1;
+        var position = 0;
+        var valueAfterZero = -1;
+        for (i in 0...50000000) {
+            position = (position + stride) % bufferLength;
+            bufferLength++;
+            position++;
+            if (position == 1) {
+                valueAfterZero = i + 1;
+            }
+        }
+        return valueAfterZero;
     }
 }
