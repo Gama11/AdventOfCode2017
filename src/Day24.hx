9df1c46ca99187e18fb9c7471a0718ad6b60136e
Jens Fischer <jensfischer95@gmail.com>
2017-12-24 15:30:29 +0100

    Day 24 part 2

--- src/Day24.hx
+++ src/Day24.hx
@@ -5,8 +5,7 @@ using Lambda;
 class Day24 extends buddy.SingleSuite {
     function new() {
         describe("Day24", {
-            it("part1", {
-                getMaxBridgeStrength(parse(
+            var input = 
 "0/2
 2/2
 2/3
@@ -14,8 +13,14 @@ class Day24 extends buddy.SingleSuite {
 3/5
 0/1
 10/1
-9/10"
-                )).should.be(31);
+9/10";
+
+            it("part1", {
+                getStrongestBridgeStrength(input).should.be(31);
+            });
+
+            it("part2", {
+                getLongestBridgeStrength(input).should.be(19);
             });
         });
     }
@@ -30,14 +35,17 @@ class Day24 extends buddy.SingleSuite {
         });
     }
 
-    function getMaxBridgeStrength(components:Array<Component>):Int {
-        function loop(bridge:Array<Int>, leftovers:Array<Component>):Int {
+    function getBestBridgeStrength(components:Array<Component>, getUtility:Bridge->Int, tieBreaker:Bridge->Int):Int {
+        function loop(bridge:Bridge, leftovers:Array<Component>):Bridge {
             var desiredPort = if (bridge.length == 0) 0 else bridge[bridge.length - 1];
             var options = leftovers.filter(c -> c.left == desiredPort || c.right == desiredPort);
             if (options.length == 0) {
-                return bridge.fold((a, b) -> a + b, 0);
+                return bridge;
             }
-            var maxStrength = 0;
+
+            var bestUtility = 0;
+            var bestBridge = [];
+
             for (option in options) {
                 var newBridge = bridge.copy();
                 if (desiredPort == option.left) {
@@ -47,16 +55,33 @@ class Day24 extends buddy.SingleSuite {
                     newBridge.push(option.right);
                     newBridge.push(option.left);
                 }
+
                 var newLeftovers = leftovers.copy();
                 newLeftovers.remove(option);
-                var strength = loop(newBridge, newLeftovers);
-                if (strength > maxStrength) {
-                    maxStrength = strength;
+                var bridge = loop(newBridge, newLeftovers);
+                var utility = getUtility(bridge);
+                if (utility > bestUtility ||
+                    (utility == bestUtility && tieBreaker(bridge) > tieBreaker(bestBridge))) {
+                    bestUtility = utility;
+                    bestBridge = bridge;
                 }
             }
-            return maxStrength;
+
+            return bestBridge;
         }
-        return loop([], components);
+        return getBridgeStrength(loop([], components));
+    }
+
+    function getStrongestBridgeStrength(input:String):Int {
+        return getBestBridgeStrength(parse(input), getBridgeStrength, null);
+    }
+
+    function getLongestBridgeStrength(input:String):Int {
+        return getBestBridgeStrength(parse(input), bridge -> bridge.length, getBridgeStrength);
+    }
+
+    function getBridgeStrength(bridge:Bridge):Int {
+        return bridge.fold((a, b) -> a + b, 0);
     }
 }
 
@@ -64,3 +89,5 @@ typedef Component = {
     final left:Int;
     final right:Int;
 }
+
+typedef Bridge = Array<Int>;
